68f7c9f2e4a42cdf5ba8b302956d79d5d5815c06
VitaliZinkevich <vitalizinkevich@gmail.com>
2018-04-07 01:31:33 +0300

    migrating to mongodb session storage and cookie with sessionID

--- app.js
+++ app.js
@@ -3,7 +3,47 @@ var app = express();
 
 var bodyParser = require('body-parser');
 var session = require('express-session');
+
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'myproject';
+
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, client) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+
+  client.close();
+});
+
+
+var MongoDBStore = require('connect-mongodb-session')(session);
 var ls = require('local-storage');
+
+var store = new MongoDBStore(
+      {
+        uri: 'mongodb://localhost:27017/connect_mongodb_session_test',
+        databaseName: 'connect_mongodb_session_test',
+        collection: 'mySessions'
+      });
+
+ // Catch errors
+ // БЫЛА ТУТ ОШИБКА ПРИ СОЗДАНИИ
+
+ store.on('error', function(error) {
+       assert.ifError(error);
+       assert.ok(false);
+     });
+
+
+
 // DELETE var jsonfile = require('jsonfile')
 var cookie = require('cookie'); // what cookies are actuallu works here
 
@@ -31,10 +71,15 @@ app.use(cookieParser())
 app.use(session({
   secret: 'keyboard cat',
   resave: false,
+  //store: store,
   saveUninitialized: true,
 
 }))
 
+app.get('/', function (req, res){
+
+  console.log (req.session.id)
+})
 
 
 app.post ('/login', function (req, res) {
@@ -46,10 +91,26 @@ const password = body.password;
  if ((username && password) && req.method === 'POST') {
    if (username === 'admin' && password === 'admin') {
 
-     let cookiesId = req.cookies['connect.sid'];
+    let options = {
+    maxAge: 1000 * 60 * 15, // would expire after 15 minutes
+    httpOnly: true // The cookie only accessible by the web server
+    //signed: true // Indicates if the cookie should be signed
+}
 
-      res.cookie ('id', cookiesId.toString())
-      ls.set ('id', cookiesId.toString())
+     let sessionId = req.session.id;
+
+     res.cookie ('sessionId', sessionId.toString(), options)
+      //ls.set ('id', cookiesId.toString())
+      console.log (req.session.id)
+
+      req.session.user = 'admin'
+
+    store.set(sessionId, req.session, function (error) {
+      if (error) {
+        console.log (error)
+      }
+    })
+
 
 
      res.send ({
@@ -73,14 +134,39 @@ const password = body.password;
 
 app.get ('/database',function  (req, res) {
 
+  store.get(req.cookies.sessionId, function (error, session) {
+
+      if (error) {
+        console.log (error)
+      } else {
+        console.log ('session FROM STORAGE ')
+          console.log ( session )
+
+      }
+
+  })
+
+console.log ('куки и сессия РАВЕНСТВО'+(req.session.id ==req.cookies.sessionId))
+console.log ('sessionId в куки '+req.cookies.sessionId)
 
        var obj = {name: 'admin', status: 'success', respond: 'message for admin only'}
-       console.log (req.cookies['connect.sid'])
-       console.log (req.cookies.id)
-      console.log (req.cookies['connect.sid'] == req.cookies.id)
+if (req.session.user == 'admin' ){
 
-       if (ls.get ('id') === req.cookies.id )
-                  {res.send (obj)}
+  res.send (obj)
+}
+
+})
+
+// (e)=>{console.log (e)}
+
+       /*if (ls.get ('id') === req.cookies.id )
+                  {res.send (obj)}*/
+
+
+
+
+app.get ('/isLoggedIn', function (req,res){
+
 
 
 })
