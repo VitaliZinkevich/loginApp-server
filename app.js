b9e93d3356c027e2a56dbb7997c6d7f83d3d5ef0
VitaliZinkevich <vitalizinkevich@gmail.com>
2018-04-06 12:51:50 +0300

    keep user sign in cookie session

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 var express = require('express');
 var app = express();
 
+var bodyParser = require('body-parser');
+var session = require('express-session');
+// DELETE var ls = require('local-storage');
+// DELETE var jsonfile = require('jsonfile')
+var cookie = require('cookie'); // what cookies are actuallu works here
 
-var faker = require('faker');
+var cookieParser = require('cookie-parser');
 
-var express = require('express');
-var app = express();
+//let crypto = require('crypto'); DELETE
+
+//var Storage = require('session-storage').create ('file', {where:'./sess_storage.json',secureKey:'secureKey'})
+
 
 var Rx = require('rxjs/Rx');
-
-var bodyParser = require('body-parser');
+var faker = require('faker');
 
 app.listen(3000, ()=>console.log('listen on 3000'));
 
@@ -18,60 +24,55 @@ app.listen(3000, ()=>console.log('listen on 3000'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(cookieParser())
+
+
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+
+}))
+
+
+
 app.post ('/login', function (req, res) {
-  
-  let body = req.body;
-  
-  const username = body.username
-  const password = body.password
-  
-  /*console.log (body.username);
-  console.log (body.password);*/
-  
+
+let body = req.body;
+const username = body.username;
+const password = body.password;
+
+ if ((username && password) && req.method === 'POST') {
+   if (username === 'admin' && password === 'admin') {
+
+     res.cookie('isLoggedIn', 'true')
+
+     res.send ({
+     'success': true,
+     'mesg':'admin zone'
+     })
+     } else  {
+       res.send ({
+         'success': false,
+         'mesg':'invalied data for admin'
+         })
+     }
+ } else {
+   res.send ({
+     'success': false,
+     'mesg':'only POST or empty data'
+     })
+ }
+})
 
 
+app.get ('/database',function  (req, res) {
 
+       var obj = {name: 'admin', status: 'success', respond: 'message for admin only'}
 
-if ((username && password) && req.method === 'POST') {
+       if (req.cookies.isLoggedIn == 'true' )
+                  {res.send (obj)}
 
-  if (username === 'admin' && password === 'admin') {
-    
-    res.send ({
-    'success': true,
-    'mesg':'admin zone'
-    })
-    
-    } else  {
-    
-      res.send ({
-        'success': false,
-        'mesg':'invalied data for admin'
-        })
-    }
-} else {
-
-  res.send ({
-    'success': false,
-    'mesg':'only POST or empty data'
-    })
-
-}
-
-
-
-/*
-  res.json( Rx.Observable.of ({
-    'randomName':faker.name.findName(),
-    'randomEmail':faker.internet.email(),
-    'randomCard': faker.helpers.createCard()
-    }));
-  console.log(res.headersSent); // true
-});*/
-
-/*
-app.get('/', function(req, res){
-  res.send('hello world');
-});
-*/
 
 })
